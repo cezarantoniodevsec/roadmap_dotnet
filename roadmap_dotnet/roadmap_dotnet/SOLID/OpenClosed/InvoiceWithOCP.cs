46a691b314cfb2b9edb8e688909fc3e79a71dcbb
Cezar Antonio <cezarantoniodev@gmail.com>
2024-12-04 09:36:11 -0300

    Open Closed Principle

---
+++ roadmap_dotnet/roadmap_dotnet/SOLID/OpenClosed/InvoiceWithOCP.cs
@@ -0,0 +1,32 @@
+﻿namespace roadmap_dotnet.SOLID.OpenClosed
+{
+    public class InvoiceWithOCP
+    {
+        public virtual double GetInvoiceDiscount(double amount)
+        {
+            return amount - 10;
+        }
+    }
+
+    public class FinalInvoice : InvoiceWithOCP
+    {
+        public override double GetInvoiceDiscount(double amount)
+        {
+            return base.GetInvoiceDiscount(amount) - 50;
+        }
+    }
+    public class ProposedInvoice : InvoiceWithOCP
+    {
+        public override double GetInvoiceDiscount(double amount)
+        {
+            return base.GetInvoiceDiscount(amount) - 40;
+        }
+    }
+    public class RecurringInvoice : InvoiceWithOCP
+    {
+        public override double GetInvoiceDiscount(double amount)
+        {
+            return base.GetInvoiceDiscount(amount) - 30;
+        }
+    }
+}
